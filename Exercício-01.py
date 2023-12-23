867ebd10cd910f846b7d12c0894805c6af44041d
lucashssilva-git <112267306+lucashssilva-git@users.noreply.github.com>
2023-12-22 21:19:53 -0300

    Exercício 01
    Exercício 01 do curso do PROZ..
    
    Existe outros exercícios anteriores que estarei postando depois, mas esse foi o primeiro que foi proposto para ser utilizado o repositório Github.

---
+++ Exercício-01.py
@@ -0,0 +1,28 @@
+# ABAIXO TEMOS A MESMA SOLUÇÃO COM UMA DINÂMICA DIFERENTE, O PRIMEIRO O CÓDIGO PRINTA NA TELA TODOS OS ANDARES.
+# JÁ NA SEGUNDA OPÇÃO OS ANDARES SÃO COLOCADOS EM UMA CONTAGEM COM A FUNÇÃO iMPORT TIME. 
+# (ESPERO OPINIÕES DE COMO POSSO MELHORAR MEU CÓDIGO E A LÓGICA. :]
+
+# for andar in range (1,22,1):
+#     if andar == 13:
+#         continue
+#     print(andar, "Andar.")
+
+###################
+
+# import time
+# print("Iniciando a contagem dos andares.. ")
+# for andar in range(0,22,1):
+#      time.sleep(1)
+#      if andar == 13:
+#          continue
+#      print(andar , "Andar")
+
+#############################################
+# Usando o loop While
+
+# andar = 20
+# while andar >= 1:
+#     if andar != 13:
+#         print("Andar:",andar)
+#     andar -= 1
+
